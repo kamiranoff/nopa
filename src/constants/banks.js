b5cfbd6f3fead132825741729364efb91f5df81e
Kevin Amiranoff <kamiranoff@gmail.com>
2017-05-01 23:56:04 +0200

    adds banks list

--- src/constants/banks.js
+++ src/constants/banks.js
@@ -8,21 +8,27 @@ const tsb = require('../../assets/banks/tsb.png');
 const banks = [{
   name: 'Barclays',
   img: barclays,
+  id: 66,
 }, {
   name: 'HSBC',
   img: hsbc,
+  id:67
 }, {
   name: 'Lloyds Bank',
   img: lloydsBank,
+  id:68
 }, {
   name: 'Natwest',
   img: natwest,
+  id: 71
 }, {
   name: 'Santander',
   img: santander,
+  id: 74
 }, {
   name: 'TSB',
   img: tsb,
+  id: 86
 },
 ]
 
